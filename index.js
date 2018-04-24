94d56eb78a1850622e369a06ec4a7b39f19edbe2
Caitlin Lauer <caitlinrinn@gmail.com>
2018-04-24 00:17:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return(`Happy holidays, ${name}!`)
+  return`Happy holidays, ${name}!`;
 }
 function happyHolidayTo(holiday, name) {
-  return('Happy ${holiday}, ${name}!')
+  return'Happy ${holiday}, ${name}!';
 }
 function holidayCountdown(holiday, days) {
-  console.log('It\'s ${days} days until ${holiday}!')
+return 'It\'s ${days} days until ${holiday}!';
 }
